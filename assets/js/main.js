c48d99265f62c57317ab340a8ef6ed35571d560e
xescnova <xescnova@gmail.com>
2022-04-03 21:02:38 +0200

    subt + xray

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,16 @@ const video = document.querySelector('.video');
 const bar = document.getElementById('progressBar');
 const playbtn = document.getElementById('playbtn');
 const videoControls = video.controls;
+var tracks = video.textTracks;
+var escenas = tracks[0];
+escenas.mode = "hidden";
 
 video.addEventListener('timeupdate', updateProgress);
 
 const videoWorks = !!document.createElement('video').canPlayType;
 if (videoWorks) {
     //video.controls = true;
-    video.muted = false;
+    video.muted = true;
 
 }
 
@@ -42,6 +45,19 @@ function botonPlay() {
         } else {
             video.pause();
             b.src = "assets/img/play.png"
+        }
+    }
+}
+
+function botonSubt() {
+    var bS = document.getElementById("botonSubt");
+    bS.onclick = function() {
+        var e = escenas.mode;
+        console.log(e);
+        if (e == "hidden") {
+            escenas.mode = "showing";
+        } else {
+            escenas.mode = "hidden";
         }
     }
 }
