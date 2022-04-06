e1a729acb211e6b1c3402c0ede376b13bbfa32cd
oka157 <kamandomar98@gmail.com>
2022-04-06 18:30:08 +0200

    Timer funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,31 @@ function volumebar() {
     }
 }
 
+function getCurrentTime(){     
+    var btnTimer = document.getElementById("sel-tiempo");
+    var tInicio= document.getElementById("startTime");
+    var tFin= document.getElementById("endTime");     
+    var tiempo=video.currentTime;     
+    if(tFin.value)
+    {
+        tFin.value = null;
+        tInicio.value = null;
+        btnTimer.textContent = "Seleccionar Tiempo";
+    }    
+    else
+    {
+        if(tInicio.value)
+        {              
+            tFin.value = tiempo;
+            btnTimer.textContent = "Reset";
+        }
+        else
+        {
+            tInicio.value = tiempo;
+        }  
+    }    
+}
+
 function plusTen() {
     video.currentTime = video.currentTime + 10;
 }
