73897de9f5f8d0ee2310f5c27093fda1ca6a651a
oka157 <kamandomar98@gmail.com>
2022-04-21 11:51:44 +0200

    Listar videos

--- listarVideos.php
+++ listarVideos.php
@@ -1,10 +1,10 @@
 <?php
 $arrFiles = array();
-$handle = opendir("/video/");
+$handle = opendir("video/");
 
 if ($handle) {
     while (($entry = readdir($handle)) !== FALSE) {
-        if (($entry != ".") && ($entry != "..") && (((strpos($entry, '.mp4') !== false) || (strpos($entry, '.jpg') !== false)))) {
+        if (($entry != ".") && ($entry != "..") && (((strpos($entry, '.mp4') !== false) || (strpos($entry, '.mkv') !== false)))) {
             $arrFiles[] = $entry;
         }
     }
