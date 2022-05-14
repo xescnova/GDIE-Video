0d983ea6054e19523812d149100fe4cac17bab3b
xescnova <xescnova@gmail.com>
2022-05-14 17:01:28 +0200

    guardar escenas funciona

--- guardarEscenas.php
+++ guardarEscenas.php
@@ -18,7 +18,7 @@ for ($i = 1; $i < sizeof($object); $i++) {
     file_put_contents($escenas,$object[$i]['id'],FILE_APPEND);
     file_put_contents($escenas,"\n",FILE_APPEND);  
     file_put_contents($escenas,decimal_to_time($object[$i]['ini']),FILE_APPEND); 
-    file_put_contents($escenas," ---> ",FILE_APPEND);
+    file_put_contents($escenas," --> ",FILE_APPEND);
     file_put_contents($escenas,decimal_to_time($object[$i]['fin']),FILE_APPEND); 
     file_put_contents($escenas,"\n",FILE_APPEND); 
     file_put_contents($escenas,$object[$i]['texto'],FILE_APPEND); 
@@ -34,6 +34,6 @@ function decimal_to_time($decimal) {
     $seconds = $decimal - (int)$decimal;
     $seconds = round($seconds * 60);
 
-    return "00:".str_pad($hours, 2, "0", STR_PAD_LEFT) . ":" . str_pad($minutes, 2, "0", STR_PAD_LEFT) . "." . str_pad($seconds, 2, "0", STR_PAD_LEFT);
+    return "00:".str_pad($hours, 2, "0", STR_PAD_LEFT) . ":" . str_pad($minutes, 2, "0", STR_PAD_LEFT) . "." . str_pad($seconds, 2, "0", STR_PAD_LEFT)."0";
 }
 ?>
